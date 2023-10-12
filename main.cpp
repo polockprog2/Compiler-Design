a7c10056ebdc96ac23040f23902e74a6143e0c60
polockprog2 <60640688+polockprog2@users.noreply.github.com>
2023-10-12 16:42:48 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,12 +12,6 @@ int main () {
       cout<<"failed to Open input file";
       return 1;
   }
-  ofstream outputFile("output.txt");
-  if(!outputFile){
-    cout<<"Failed to open output file.";
-    return 1;
-  }
-
   string line;
   while(getline(inputFile,line)){
     string operators;
@@ -26,8 +20,7 @@ int main () {
         if(c == '+' || c == '-' || c == '*' || c == '/' || c == '%')
             operators +=c;
     }
-    outputFile<<operators;
-    cout<<operators;
+    cout<<operators<<endl;;
   }
 
 }
